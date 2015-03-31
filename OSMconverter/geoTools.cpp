ce44da195b34fece26f06839ad535be3d41c06b7
CagkanToptas <cagkan.toptas@mail.polimi.it>
2015-03-31 04:58:57 +0200

    BingAerial Texture Implemented / [Error on Height Calculation]

--- OSMconverter/geoTools.cpp
+++ OSMconverter/geoTools.cpp
@@ -1,8 +1,11 @@
 #include <vector>
 #include <iostream>
 #include <math.h>
+#include <curl/curl.h>
 #include <fstream>
 #include "geoTools.h"
+#include <string>
+
 
 using namespace std;
 
@@ -17,20 +20,171 @@ coordinate MercatorProjection::LatLontoMeters(double lat, double lon)
 	return coordinate{ meterx, meterz };
 }
 
-coordinate MercatorProjection::meterstoLatLon(double mx, double my)
+coordinate MercatorProjection::meterstoLatLon(double meterx, double meterz)
 {
 	coordinate coord;
 
 	double lat, lon;
 
-	lon = (my / originShift) * 180.0;
-	lat = (mx / originShift) * 180.0;
+	lon = (meterz / originShift) * 180.0;
+	lat = (meterx / originShift) * 180.0;
 	lat = 180 / PI * (2 * atan(exp(lat * PI / 180.0)) - PI / 2.0);
 
 	coord.meterx = lat;
 	coord.meterz = lon;
 	return coord;  // (lat , Lon)
 }
+
+Tuple MercatorProjection::metertoPixel(double meterx, double meterz, int zoom)
+{
+
+	double res = Resolution(zoom);
+	double pixelX, pixelY;
+
+	pixelY = (meterx + originShift) / res;
+	pixelX = (meterz + originShift) / res;
+
+	return Tuple(pixelX, pixelY);
+
+}
+
+coordinate MercatorProjection::pixeltoMeter(double pixelX, double pixelY, int zoom)
+{
+	double res = Resolution(zoom);
+	double meterX = (pixelY * res) - originShift;
+	double meterZ = (pixelX * res) - originShift;
+
+	coordinate coord;
+	coord.meterx = meterX;
+	coord.meterz = meterZ;
+	return coord;
+}
+
+Tuple MercatorProjection::PixelToTile(double pixelX, double pixelY)
+{
+	int Tilex = (int)(ceil(pixelX / (double)tileSize)) -1;
+	int Tiley = (int)(ceil(pixelY / (double)tileSize)) -1;
+
+	return Tuple(Tilex, Tiley);
+}
+
+size_t write_data(void *ptr, size_t size, size_t nmemb, FILE *stream)
+{
+	size_t written;
+	written = fwrite(ptr, size, nmemb, stream);
+	return written;
+}
+
+AerialImageLoader::AerialImageLoader()
+{
+
+	apiKEY = "Ajk_KcXsfD1lOygMwZcc1m3-1nwh_qugU1rQQuDyWGVfJ3yjld16xJrdXm7kUJqM";
+	cacheFolder = "BingAerial/";
+
+
+}
+
+std::string AerialImageLoader::getTileURL(BBox bbox, int pixelX, int pixelY)
+{
+	string server = "http://dev.virtualearth.net/REST/v1/Imagery/Map/Aerial?";
+	string mapSize = to_string(pixelX) + "," + to_string(pixelY);
+	string mapArea = std::to_string(bbox.bottom) + "," + std::to_string(bbox.left) + "," + std::to_string(bbox.top) + "," + std::to_string(bbox.right);
+	string URL = server + "maparea=" + mapArea + "&ms=" + mapSize + "&key=" + apiKEY;
+
+	return URL;
+}
+
+string AerialImageLoader::getTileURL2(coordinate centercoord, int pixelX, int pixelY)
+{
+	string server = "http://dev.virtualearth.net/REST/v1/Imagery/Map/Aerial/";
+	string centerpoint = to_string(centercoord.meterx) + "," + to_string(centercoord.meterz);
+	string zoomLevel = "19";
+	string mapSize = to_string(pixelX) + "," + to_string(pixelY);
+	string URL = server + centerpoint + "/" + zoomLevel + "?mapsize=" + mapSize + "&key=" + apiKEY;
+
+	return URL;
+}
+
+void AerialImageLoader::downloadAerials(Terrain *terrain, HeightmapInfo *heightmapinfo)
+{
+
+	double delta = 1.0 / 1200.0;
+	MercatorProjection proj;
+
+	for (int i = terrain->top+1; i <= terrain->bottom; i += 2)
+	{
+		for (int j = terrain->left+1; j <= terrain->right; j += 2)
+		{
+			coordinate centerCoordlatlon;
+			centerCoordlatlon.meterx = (double)heightmapinfo->Lat + 1.0 - delta * i;
+			centerCoordlatlon.meterz = (double)heightmapinfo->Lon + delta * j;
+
+			coordinate bottomleftCoordlatlon;
+			bottomleftCoordlatlon.meterx = (double)heightmapinfo->Lat + 1.0 - delta * (i + 1);
+			bottomleftCoordlatlon.meterz = (double)heightmapinfo->Lon + delta * (j - 1);
+			coordinate toprightCoordlatlon;
+			toprightCoordlatlon.meterx = heightmapinfo->Lat + 1 - delta * (i - 1);
+			toprightCoordlatlon.meterz = heightmapinfo->Lon + delta * (j + 1);
+
+			coordinate bottomleftmeter = proj.LatLontoMeters(bottomleftCoordlatlon.meterx, bottomleftCoordlatlon.meterz);
+			Tuple bottomleftpixel = proj.metertoPixel(bottomleftmeter.meterx, bottomleftmeter.meterz,19);
+			coordinate toprightmeter = proj.LatLontoMeters(toprightCoordlatlon.meterx, toprightCoordlatlon.meterz);
+			Tuple toprightpixel = proj.metertoPixel(toprightmeter.meterx, toprightmeter.meterz, 19);
+
+			int pixelX = (int)abs(bottomleftpixel.x - toprightpixel.x);
+			int pixelY = (int)abs(bottomleftpixel.y - toprightpixel.y);
+
+
+			string url = getTileURL2(centerCoordlatlon, pixelX, pixelY);
+			string fileName = "Bing" + to_string(heightmapinfo->Lat) + "_" + to_string(heightmapinfo->Lon) + "H" + to_string(j-1) + "_" + to_string(i-1) + ".jpg";
+			downloadAndSaveImage(url, fileName);
+
+		}
+	}
+
+}
+
+bool AerialImageLoader::downloadAndSaveImage(std::string url, std::string FileNametoSave)
+{
+	//check if file already exists
+	std::string outputPath = cacheFolder + FileNametoSave;
+
+	std::ifstream fin(outputPath.c_str());
+	if (!fin.fail())
+	{
+		fin.close();
+		cout << "Aerial image is exists on the cache" << endl;
+		return true;
+	}
+
+	CURL *curl;
+	FILE *fp;
+	CURLcode res;
+	curl = curl_easy_init();
+	if (curl) 
+	{
+		try
+		{
+			fp = fopen(outputPath.c_str(), "wb");
+			curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
+			curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_data);
+			curl_easy_setopt(curl, CURLOPT_WRITEDATA, fp);
+			res = curl_easy_perform(curl);
+			/* always cleanup */
+			curl_easy_cleanup(curl);
+			fclose(fp);
+		}
+		catch (exception e)
+		{
+			cout << "Problem occured getting Image from Server" << endl;
+			return false;
+		}
+	}
+
+
+	return true;
+}
+
 
 void TerrainLoader::loadheightmap(char* filename)
 {
@@ -121,12 +275,69 @@ double TerrainLoader::getTerrainTriangleHeight(double meterx, double meterz)
 {
 		double finalvalue;
 
+		MercatorProjection proj;
+		coordinate c = proj.meterstoLatLon(meterx, meterz);
 
-		double ratioX = (heightMapInfo.coordmax.meterx - meterx) / heightMapInfo.height;
-		double ratioZ = (meterz - heightMapInfo.coordmin.meterz) / heightMapInfo.width;
+		double ratioX = heightMapInfo.Lat + 1.0 - c.meterx;
+		double ratioZ = c.meterz - heightMapInfo.Lon;
+
+		//WRONG CALCULATION
+		double ratioXwro = (heightMapInfo.coordmax.meterx - meterx) / heightMapInfo.height;
+		double ratioZwro = (meterz - heightMapInfo.coordmin.meterz) / heightMapInfo.width;
 	
-		double Xindex = (1200 * ratioX);
-		double Zindex = (1200 * ratioZ);
+		double Xindexwro = 1200.0 * ratioXwro;
+		double Zindexwro = 1200.0 * ratioZwro;
+
+		double Xindex = (1200.0 * ratioX);
+		double Zindex = (1200.0 * ratioZ);
+		double delta = 1.0 / 1200.0;
+
+
+
+		////Upper Triangle
+		//if ((Xindex - floor(Xindex)) + (Zindex - floor(Zindex)) < 1.0f)
+		//{
+		//	int n = heightmap[(int)floor(Xindex)][(int)ceil(Zindex)];
+		//	int m = heightmap[(int)floor(Xindex)][(int)floor(Zindex)];
+		//	int k = heightmap[(int)ceil(Xindex)][(int)floor(Zindex)];
+		//	double interpolationUp = m + (n - m) * (Zindex - floor(Zindex));
+		//	double interpolationHypothenus = k + (n - k) * (Zindex - floor(Zindex));
+		//
+		//	//double Xtop = heightMapInfo.coordmax.meterx - floor(Xindex) * heightMapInfo.sizeX;
+		//	//double a = Xtop - meterx;
+		//	//double aplusb = heightMapInfo.sizeX * (ceil(Zindex) - Zindex);
+		//	
+		//	double Xtop = heightMapInfo.Lat + 1 - floor(Xindex) * delta;
+		//	double a = Xtop - c.meterx;
+		//	double aplusb = delta * (ceil(Zindex) - Zindex);
+		//	
+		//	finalvalue = interpolationUp + (interpolationHypothenus - interpolationUp) * (a / aplusb);
+		//}
+
+		////Lower Triangle
+		//else if ((Xindex - floor(Xindex)) + (Zindex - floor(Zindex)) >= 1.0f)
+		//{
+		//	int n = heightmap[(int)ceil(Xindex)][(int)ceil(Zindex)];
+		//	int m = heightmap[(int)ceil(Xindex)][(int)floor(Zindex)];
+		//	int k = heightmap[(int)floor(Xindex)][(int)ceil(Zindex)];
+		//	double interpolationDown = m + (n - m) * (Zindex - floor(Zindex));
+		//	double interpolationHypothenus = m + (k - m) * (Zindex - floor(Zindex));
+		//		
+		//	//double Xbottom = heightMapInfo.coordmax.meterx - ceil(Xindex) * heightMapInfo.sizeX;
+		//	//double a = meterx - Xbottom;
+		//	//double aplusb = heightMapInfo.sizeX * (Zindex - floor(Zindex));
+
+		//	double Xbottom = heightMapInfo.Lat + 1 - ceil(Xindex) * delta;
+		//	double a = c.meterx - Xbottom;
+		//	double aplusb = delta * (Zindex - floor(Zindex));
+
+		//	finalvalue = interpolationDown + (interpolationHypothenus - interpolationDown) * (a / aplusb);
+		//}
+
+		//[ALTERNATE METHOD]
+
+	
+
 
 		//Upper Triangle
 		if ((Xindex - floor(Xindex)) + (Zindex - floor(Zindex)) < 1.0f)
@@ -137,59 +348,31 @@ double TerrainLoader::getTerrainTriangleHeight(double meterx, double meterz)
 			double interpolationUp = m + (n - m) * (Zindex - floor(Zindex));
 			double interpolationHypothenus = k + (n - k) * (Zindex - floor(Zindex));
 		
-			double Xtop = heightMapInfo.coordmax.meterx - floor(Xindex) * heightMapInfo.sizeX;
-			double a = Xtop - meterx;
-			double aplusb = heightMapInfo.sizeX * (ceil(Zindex) - Zindex);
+			double a = Xindex - floor(Xindex);
+			double aplusb = (ceil(Zindex) - Zindex);
 			finalvalue = interpolationUp + (interpolationHypothenus - interpolationUp) * (a / aplusb);
-			//return finalvalue;
 
 		}
-
+		
 		//Lower Triangle
 		else if ((Xindex - floor(Xindex)) + (Zindex - floor(Zindex)) >= 1.0f)
 		{
+
 			int n = heightmap[(int)ceil(Xindex)][(int)ceil(Zindex)];
 			int m = heightmap[(int)ceil(Xindex)][(int)floor(Zindex)];
 			int k = heightmap[(int)floor(Xindex)][(int)ceil(Zindex)];
 			double interpolationDown = m + (n - m) * (Zindex - floor(Zindex));
 			double interpolationHypothenus = m + (k - m) * (Zindex - floor(Zindex));
 				
-			double Xbottom = heightMapInfo.coordmax.meterx - ceil(Xindex) * heightMapInfo.sizeX;
-			double a = meterx - Xbottom;
-			double aplusb = heightMapInfo.sizeX * (Zindex - floor(Zindex));
+			double a = ceil(Xindex) - Xindex;
+			double aplusb = Zindex - floor(Zindex);
 			finalvalue = interpolationDown + (interpolationHypothenus - interpolationDown) * (a / aplusb);
-			//return finalvalue;
-		}
+		
+			if ((Xindex - floor(Xindex)) + (Zindex - floor(Zindex)) > 0.98f && (Xindex - floor(Xindex)) + (Zindex - floor(Zindex)) < 1.02f)
+				return interpolationHypothenus;
 
-		//[ALTERNATE METHOD]
-		//double finalvalue2;
-		////Upper Triangle
-		//if ((Xindex - floor(Xindex)) + (Zindex - floor(Zindex)) < 1.0f)
-		//{
-		//	int n = heightmap[(int)floor(Xindex)][(int)ceil(Zindex)];
-		//	int m = heightmap[(int)floor(Xindex)][(int)floor(Zindex)];
-		//	int k = heightmap[(int)ceil(Xindex)][(int)floor(Zindex)];
-		//	double interpolationUp = m + (n - m) * (Zindex - floor(Zindex));
-		//	double interpolationHypothenus = k + (n - k) * (Zindex - floor(Zindex));
-		//
-		//	double a = Xindex - floor(Xindex);
-		//	double aplusb = (ceil(Zindex) - Zindex);
-		//	finalvalue2 = interpolationUp + (interpolationHypothenus - interpolationUp) * (a / aplusb);
-		//}
-		//
-		////Lower Triangle
-		//else if ((Xindex - floor(Xindex)) + (Zindex - floor(Zindex)) >= 1.0f)
-		//{
-		//	int n = heightmap[(int)ceil(Xindex)][(int)ceil(Zindex)];
-		//	int m = heightmap[(int)ceil(Xindex)][(int)floor(Zindex)];
-		//	int k = heightmap[(int)floor(Xindex)][(int)ceil(Zindex)];
-		//	double interpolationDown = m + (n - m) * (Zindex - floor(Zindex));
-		//	double interpolationHypothenus = m + (k - m) * (Zindex - floor(Zindex));
-		//		
-		//	double a = ceil(Xindex) - Xindex;
-		//	double aplusb = Zindex - floor(Zindex);
-		//	finalvalue2 = interpolationDown + (interpolationHypothenus - interpolationDown) * (a / aplusb);
-		//}
+		}
+			
 
 		return finalvalue;
 }
--- OSMconverter/geoTools.h
+++ OSMconverter/geoTools.h
@@ -1,5 +1,6 @@
 #pragma once
 #include "OSMparser.h"
+#include "gltools.h"
 # define PI 3.14159265358979323846
 
 
@@ -36,11 +37,65 @@ class MercatorProjection
 
 	
 public:
-	const double originShift = PI * 6378137;
+	
+	MercatorProjection()
+	{
+		tileSize = 256;
+		initialResolution = 2.0 * (double)PI * 6378137.0 / (double)tileSize;
+	}
+
 	//"Converts given lat/lon in WGS84 Datum to XY in Spherical Mercator EPSG:900913"
 	coordinate LatLontoMeters(double lat, double lon);
-	//"Converts XY point from Spherical Mercator EPSG:900913 to lat/lon in WGS84 Datum [NOT WORK WELL]" 
+	//"Converts XY point from Spherical Mercator EPSG:900913 to lat/lon in WGS84 Datum" 
 	coordinate meterstoLatLon(double meterx, double meterz);
+
+	//"Converts EPSG:900913 to pyramid pixel coordinates in given zoom level"
+	Tuple metertoPixel(double meterx, double meterz, int zoom);
+
+	//Converts pixel coordinates in given zoom level of pyramid to EPSG:900913 
+	coordinate pixeltoMeter(double pixelX, double pixelZ, int zoom);
+
+	//"Returns a tile covering region in given pixel coordinates"
+	Tuple PixelToTile(double pixelX, double pixelZ);
+
+private:
+	const double originShift = PI * 6378137.0;
+	int tileSize;
+	double initialResolution;
+
+	//"Resolution (meters/pixel) for given zoom level (measured at Equator)"
+	double Resolution(int zoom)
+	{
+		return initialResolution / (double)pow(2, zoom);
+	}
+
+
+};
+
+
+
+class AerialImageLoader
+{
+
+public :
+	AerialImageLoader();
+
+	//Saves aerial images to the BingAerial Folder
+	bool downloadAndSaveImage(std::string url, std::string fileName);
+
+	//generate tile URL from bounding box
+	std::string getTileURL(BBox bbox, int pixelsizeX, int pixelsizeY);
+
+	//generate tile URL from centerpoint and specified zoom level (19)
+	std::string getTileURL2(coordinate centercoord, int pixelX, int pixelY);
+
+	//download necessary aerials according to OSM bbox
+	void downloadAerials(Terrain *terrain, HeightmapInfo *heightmapinfo);
+
+private:
+	std::string apiKEY;
+	std::string cacheFolder;
+
 };
 
 
@@ -48,16 +103,26 @@ class TerrainLoader
 {
 
 public:
+
 	short heightmap[1201][1201];
+
+	//Hold information about height map loaded
 	HeightmapInfo heightMapInfo;
+
+	//load height map to the memory given
 	void loadheightmap(char* filename);
 
-	double getTerrainTriangleHeight(double meterx, double meterz);
-
-
+	//Get the height of terrain given lat/lon value
 	double getTerrainHeight(double lat, double lon);
+
+	//Get the height of terrain given meter coordinate
 	double getTerrainHeight2(double meterx, double meterz);
 private:
+
+	//Convert heightmap entries from BigEndian to LittleEndian for windows
 	short convertLittleEndian(short num);
+
+	//interpolate triangle for obtain inner height value in triangle
+	double getTerrainTriangleHeight(double meterx, double meterz);
 
 };
